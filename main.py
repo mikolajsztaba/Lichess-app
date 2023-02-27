82948327738e6f6ea5c7cc29b4e6e347c59c49b4
mikolajsztaba <mikolaj25sztaba@gmail.com>
2023-02-27 21:36:42 +0100

    First commit of lichess api application

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+
+# asking about username
+# username = input('Provide your username:\n')
+
+# api request
+# response = requests.get(f"https://lichess.org/api/user/{username}")
+response = requests.get(f"https://lichess.org/api/user/PAPOR123")
+
+# return code
+print(response)
+json_return = response.json()
+print(json_return)
+print(json_return['id'])
+# print(response.json())
