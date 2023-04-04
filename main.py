53daaa935add8f61b42192f34c9eca3d06f9f82e
mikolajsztaba <mikolaj25sztaba@gmail.com>
2023-04-04 22:32:14 +0200

    Added request by nickname provided by the user and handling labels by mentioned nickname.

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import requests
 import tkinter as tk
 
-# asking about username
-# username = input('Provide your username:\n')
 
+# setting nickname in the label and request to lichess api
 def get_nickname():
-    print(user_input.get())
+    user_nick = user_input.get()
+    api_response = requests.get(f"https://lichess.org/api/user/{user_nick}")
+    print(api_response)
+    print(api_response.status_code)
+    if api_response.status_code == 200:
+        label_nick.configure(text=user_nick)
+    else:
+        label_nick.configure(text="Invalid nickname")
+
 
 # api request
 # response = requests.get(f"https://lichess.org/api/user/{username}")
@@ -24,7 +31,6 @@ master = tk.Tk()
 
 user_input = tk.StringVar(master)
 
-
 # test labels
 label_nick = tk.Label(
     text=f"{json_return['id']}",
@@ -40,14 +46,14 @@ label_test = tk.Label(
     width=10,
     height=10
 )
-tk.Label(master, text="First Name").grid(row=0)
-tk.Label(master, text="Last Name").grid(row=1)
+tk.Label(master, text="Lichess nickname").grid(row=0)
+# tk.Label(master, text="Last Name").grid(row=1)
 
 e1 = tk.Entry(master, textvariable=user_input)
-e2 = tk.Entry(master)
+# e2 = tk.Entry(master)
 
 e1.grid(row=0, column=1)
-e2.grid(row=1, column=1)
+# e2.grid(row=1, column=1)
 label_nick.grid(row=2, columnspan=3)
 
 tk.Button(master, text="Click to Show", command=get_nickname).grid(row=3)
